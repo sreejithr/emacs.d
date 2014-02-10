2df4475fa26c784b13415ef17c050c961e9f99ca
Sreejith Jr <r.sreejith@mquotient.net>
2014-02-10 15:10:42 +0530

    Added lines for 80 column margin line

--- init.el
+++ init.el
@@ -303,3 +303,10 @@
 
 (global-set-key [f11] 'fullscreen)
 
+;; 80 Column Line
+(add-to-list 'load-path "~/.emacs.d/fill-column-indicator-1.83")
+(require 'fill-column-indicator)
+(define-globalized-minor-mode
+  global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode t)
+
